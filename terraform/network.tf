18f29a30717d39d77125640d994f4324d606d4a4
Niko Raes <niko.raes@arcadis.com>
2025-10-19 19:27:13 +0200

    Update ArgoCD configuration: upgrade ArgoCD version, add nodeSelector for GKE spot nodes, and remove unused patches

--- terraform/network.tf
+++ terraform/network.tf
@@ -9,3 +9,24 @@ resource "google_compute_subnetwork" "subnet" {
   region        = var.region
   network       = google_compute_network.vpc_network.id
 }
+
+// Cloud Router for NAT (required for private GKE nodes to pull external images)
+resource "google_compute_router" "router" {
+  name    = "${var.network_name}-router"
+  region  = var.region
+  network = google_compute_network.vpc_network.id
+}
+
+// Cloud NAT for egress traffic from private GKE nodes
+resource "google_compute_router_nat" "nat" {
+  name                               = "${var.network_name}-nat"
+  router                             = google_compute_router.router.name
+  region                             = var.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
